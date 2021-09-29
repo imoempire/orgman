fac98da191c039d4dae837aa5bfa0f5ae7eb734b
imoempire <mrimoempire@gmail.com>
2021-09-29 08:03:52 +0000

    add sidebar data and render components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import SideBar from './Components/SideBar/SideBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <SideBar />
       <h1>Testing App</h1>
+
     </div>
   );
 }
---
+++ src/Components/SideBar/SideBarData.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const SideBarData = [
+    {
+        Tab: "Home",
+        Link: "/Home"
+    },
+    {
+        Tab: "Manage",
+        Link: "/mange"
+    },
+    {
+        Tab: "To-do",
+        Link: "/todo"
+    },
+    {
+        Tab: "Note",
+        Link: "/note"
+    }
+]
+    
