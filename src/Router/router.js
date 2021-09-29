06e1a526babffeef4b500c65e167245d389b5640
imoempire <mrimoempire@gmail.com>
2021-09-29 15:03:26 +0000

    change of route and component positions

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,21 +1,44 @@
 
 import React from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import App from '../App'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Nav from '../Components/Nav'
+import Todo from '../Components/Todo/Todo';
+import Meet from '.././Meet/Meet';
+// import InnerBar from '../Components/InnerBar';
 import Navbar from '../Components/Navbar/Navbar';
-import { SideBarData } from '../Components/SideBar/SideBarData';
+// import SideBar from '../Components/SideBar/SideBar';
+import App from '../App';
 
 const router = () => {
     return (
         <BrowserRouter>
-            <Navbar/>
-            <SideBarData/>
+               
+        <Navbar/>
+        <div class="columns is-variable is-0">
+        <div>
+        <div class="side-container">
+        <div className="menu-wrapper py-1">
+         <App/>
+          </div>
+        </div>
+        </div>
+
+        <div class="column nav">
+          <div className="nav">
             <Switch>
-                <Route exact path="/" component={App} />
-                {/* <Route path='' component={} /> */}
-                {/* <Route path='' component={} /> */}
-                {/* <Route path='' component={} /> */}
+                <Route exact path="/" component={Todo} />
+                <Route exact path="/meet" component={Meet} />
             </Switch>
+          </div>
+          </div>
+
+      </div>
+
+
+
+
+
+
         </BrowserRouter>
     );
 }
