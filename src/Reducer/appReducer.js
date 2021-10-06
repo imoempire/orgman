e4248a8f8dbdb481199365f1d7b225b13d57748a
imoempire <mrimoempire@gmail.com>
2021-10-06 13:04:57 +0000

    remove old cases

--- src/Reducer/appReducer.js
+++ src/Reducer/appReducer.js
@@ -1,18 +1,12 @@
 import { ADD_LIST, DELETE_LIST, EDIT_LIST } from "../Action/appActions";
-import {ADD_NOTE, DELETE_NOTE, EDIT_NOTE} from '../Action/NotesActions'
 
 
 const initialState ={
     Todo: [
        { list: 'clean car',
-        id: '4567ioewjh2367'}
+         note: 'wqefe',
+         id: '4567ioewjh2367'}
     ],
-    Note:[
-        {   note: 'wqefe',
-            id: '145353'
-
-        }
-    ]
 }
 
 const appReducer = ( state=initialState, action)=>{
@@ -32,25 +26,6 @@ const appReducer = ( state=initialState, action)=>{
         case EDIT_LIST:
             return{...state, Todo: state.Todo.map((todo)=>
                 todo.id === action.payload.id ? action.payload : todo 
-                )}
-        
-        // NOte 
-
-        // Add note code
-        case ADD_NOTE :
-            return {...state.Note, Note: [...state.Note, action.payload] }
-       
-            // Delete note code
-        case DELETE_NOTE:
-            const SavedList = state.Note.filter((note)=>{
-                return note.id !== action.payload;
-            });
-            return {...state, Note: SavedList }
-        
-            // edit note code
-        case EDIT_NOTE:
-            return{...state, Note: state.Note.map((note)=>
-                note.id === action.payload.id ? action.payload : note 
                 )}
 
         default:
